964b7456501842c383aa52acdb34a391924ad983
Burak Gençer <bggithub@proton.me>
2025-03-08 22:53:20 +0300

    Example 2

---
+++ 0_kurulumlar/aciklamalar.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  print("Hello World");
+  print("Hello' Dart");
+  print('Hello\' Dart');
+  print("3 * 5");
+  print(3 * 5);
+
+  // + işaretleri metinleri birleştirir
+  print("hello " + "world");
+
+  /* yorum bloğu
+  print("deneme");
+  print("deneme");
+  print("deneme");
+  */
+  }
