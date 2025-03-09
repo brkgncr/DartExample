5f6472d174fada0e98484bca6dda7aec43987da3
Burak Gençer <bggithub@proton.me>
2025-03-09 21:07:32 +0300

    Example 3

---
+++ 1_degisken_veri_tipleri/number_veri_tipi.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int yas = 36;
+  yas = 40;
+  print(yas);
+
+  double sicaklik = 50.7;
+  print(sicaklik);
+
+  num kilo = 40;
+  print(kilo);
+
+  var yil = 1955;
+  yil = 50.5.toInt();
+  print(yil);
+}
