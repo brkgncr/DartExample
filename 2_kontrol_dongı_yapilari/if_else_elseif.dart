bf89552b92f8bc5b0388d8f2aa985dd8ee98029c
Burak Gençer <bggithub@proton.me>
2025-03-15 23:58:15 +0300

    Example 6

---
+++ 2_kontrol_dongı_yapilari/if_else_elseif.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+ int sayi1 = 10, sayi2 = 8;
+
+/*  if(sayi1 > sayi2) {
+  print("$sayi1 sayısı $sayi2 saysından büyüktür");
+ } else {
+  print("$sayi1 sayısı $sayi2 sayısından küçüktür");
+ } */
+
+/*   if(sayi1 > sayi2) {
+  print("$sayi1 sayısı $sayi2 saysından büyüktür");
+ } else if (sayi1 < sayi2) {
+  print("$sayi1 sayısı $sayi2 sayısından küçüktür");
+ } else {
+  print("Sayılar eşit");
+ } */
+
+ if(sayi1 > sayi2) {
+  print("$sayi1 sayısı $sayi2 saysından büyüktür");
+ } 
+ if (sayi1 < sayi2) {
+  print("$sayi1 sayısı $sayi2 sayısından küçüktür");
+ }  
+ if (sayi1 == sayi2){
+  print("Sayılar eşit");
+ }
+}
